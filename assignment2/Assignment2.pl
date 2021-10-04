0d4fe3d6f28c3234fe9db4ae365c898ecf450cda
Johan Israelsson -Desktop <johan4411@gmail.com>
2021-10-04 12:12:15 +0200

    Final fixes for sictus.

--- assignment2/Assignment2.pl
+++ assignment2/Assignment2.pl
@@ -1,12 +1,7 @@
-% guess_a_word/0.
-% starts the game, calls the predicate game(yes) 
+
 
 guess_a_word:-game(yes).
 
-
-% game(+X) starts a new round if X is yes. 
-% If X is no the program ends. 
-% If X has another value a correct value is asked for.  
 
 game(no):-
   write('Thanks for a good game!'),nl.
@@ -25,21 +20,17 @@ game(X):-
   game(Answer).
 
 
-% read_Word(-List) reads the Word that the game leader gives and that   
-% the player should guess. Except the reading the predicate translates 
-% the atom to a list of letters 
+
 
 read_word(List):-
   write('The game leader gives its word'),
   nl,
   read(Word),
-  name(Word,CharList),
-  letters(CharList,List).
+  name(Word,ReadList),
+  letters(ReadList,List).
 
 
-% letters(+AsciList,-LetterList) translates an ascii list  
-% to a list with the corresponding letters. 
-% This recursive program utilises the built-in predicate name/2
+
 letters([],[]).
 letters([A],[Z]):-
     name(Z,[A]).
@@ -47,8 +38,7 @@ letters([Z|X],[E|Y]):-
     name(E,[Z]),
     letters(X,Y).
 
-% read_guess(-List) reads the word the player guesses 
-% and translates it to a list of letters 
+
 read_guess(List):-
   write('Player guess a word'),nl,
   read(Word),
@@ -56,10 +46,7 @@ read_guess(List):-
   letters(CharList,List).
 
 
- 
-% play(+X,+Y) if X and Y are equal, play succeeds directly. 
-% If the X and Y differ, they should be compared, the comparison 
-% presented and a new try should be performed. Note that the words may % be of different lengths. 
+
 
 play(Word,Word):-
   write('Correct!'),nl.
@@ -73,18 +60,12 @@ play(Word1,Word2):-
   play(Word1,NewWord).
 
 
-
-% same_placing(+Word1,+Word2,-Starlist)  
-% checks for the letters that have the right placing.  
-% The predicate produces a list with stars for positions where the letters are 
-% not the correct ones. The correct placed letters is included. 
-% Exemple: The guess is winter and the correct word is summer  
-% the Starlist will be: [*,*,*,*,e,r]
 same_placing([],[],[]).
 same_placing([],A,[]).
+same_placing([A],[A],[A]).
 
-same_placing([X|Word1],[X|Word2],[Y|Starlist]):- 
-    name(Y,[X]),
+same_placing([X|Word1],[X|Word2],[X|Starlist]):- 
+
     same_placing(Word1,Word2,Starlist).
 
 same_placing([Z|Word1],[Y|Word2],[*|Starlist]):- 
@@ -92,11 +73,8 @@ same_placing([Z|Word1],[Y|Word2],[*|Starlist]):-
 
 same_placing([W|Word1],[],[*|Starlist]):- 
     same_placing(Word1,[],Starlist).
-% included_but_not_same_placing(+Word1,+Word2,-List) 
-% will check the guessed letters that can be found in the  
-% correct word but not in the right position
-member(X,[X|List]).
-member(X,[E|List]):- member(X,List).
+
+
 members([],X,[]).
 members([X|Rest],List,[X|Output]):-
     member(X,List),!,
@@ -104,6 +82,15 @@ members([X|Rest],List,[X|Output]):-
 
 members([X|Rest],List,Output):-
     members(Rest,List,Output).
+
+not_members([],[]).
+not_members([X|Rest],Output):-
+    member(X,Rest),!,
+    not_members(Rest,Output).
+
+not_members([X|Rest],[X|Output]):-
+    not_members(Rest,Output).
+
 
 not_in_starlist(X,[],[]).
 not_in_starlist([],[X],[X]).
@@ -123,15 +110,12 @@ included_but_not_same_placing(Correct,Guess,Remainder):-
     not_in_starlist(Starlist,Correct,NotInStarList),
     members(Guess,NotInStarList,Remainder).
 
-
-
-% output(+Starlist,+List) should write the message to  
-% player presenting the result.
 output(Starlist,[]):- 
     write(Starlist),nl.
 
 output(Starlist,List):- 
     write(Starlist),write('        Occurring letters:'),
-    write(List),nl.
+    not_members(List,Output),
+    write(Output),nl.
 
 
